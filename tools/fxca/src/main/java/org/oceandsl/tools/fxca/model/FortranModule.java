1eae22de75ab11528d83dcfc35070158a2e4be86
Henning Schnoor <hs@informatik.uni-kiel.de>
2023-02-24 10:25:46 +0100

    debug checkstyle

--- tools/fxca/src/main/java/org/oceandsl/tools/fxca/model/FortranModule.java
+++ tools/fxca/src/main/java/org/oceandsl/tools/fxca/model/FortranModule.java
@@ -1,3 +1,24 @@
+/***************************************************************************
+ * Copyright (C) 2023 OceanDSL (https://oceandsl.uni-kiel.de)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ***************************************************************************/
+
+/**
+ * @author Henning Schnoor
+ *
+ */
+
 package org.oceandsl.tools.fxca.model;
 
 import java.io.IOException;
@@ -14,23 +35,25 @@ import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 
+//CHECKSTYLE:OFF
 import org.oceandsl.tools.fxca.tools.ListTools;
 import org.oceandsl.tools.fxca.tools.Pair;
 import org.w3c.dom.Document;
 import org.w3c.dom.Node;
 import org.xml.sax.SAXException;
+//CHECKSTYLE:ON
 
 import lombok.Getter;
 
 public class FortranModule {
 
-	@Getter Path xmlFilePath;
-	@Getter Set<String> usedModules;
+	@Getter private Path xmlFilePath;
+	@Getter private Set<String> usedModules;
 	// @Getter Set<String> specifiedSubroutines;
 	// @Getter Set<String> specifiedFunctions;
-	@Getter Set<String> specifiedOperations;
-	@Getter String moduleName;
-	@Getter boolean namedModule;
+	@Getter private Set<String> specifiedOperations;
+	@Getter private String moduleName;
+	@Getter private boolean namedModule;
 	StatementNode documentElement;
 	
 	public FortranModule(Path xmlFilePath) throws ParserConfigurationException, SAXException, IOException { 
@@ -71,15 +94,19 @@ public class FortranModule {
 		System.out.println("# nodes: " + documentElement.allDescendents(node -> true, true).size());
 	}
 	
+	/*
 	@Deprecated
-	public Set<String> computeSubroutineDeclarations() throws ParserConfigurationException, SAXException, IOException {
+	private Set<String> computeSubroutineDeclarations() throws ParserConfigurationException, SAXException, IOException {
 		return documentElement.getDescendentAttributes(StatementNode.isSubroutineStatement, subroutineNode -> StatementNode.getNameOfOperation(subroutineNode));
 	}
+	*/
 	
+    /*
 	@Deprecated
 	private Set<String> computeFunctionDeclarations() throws ParserConfigurationException, SAXException, IOException {
 		return documentElement.getDescendentAttributes(StatementNode.isFunctionStatement, functionNode -> StatementNode.getNameOfOperation(functionNode));
 	}
+	*/
 	
 	public Set<String> computeOperationDeclarations() throws ParserConfigurationException, SAXException, IOException {
 		return documentElement.getDescendentAttributes(StatementNode.isOperationStatement, operationNode -> StatementNode.getNameOfOperation(operationNode));
