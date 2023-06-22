29d9e4a4ca9a45e7007f849694ffd89eb5db96f3
Reiner Jung <reiner.jung@email.uni-kiel.de>
2023-06-22 08:29:22 +0200

    Updated maa tool to support interface merging.

--- tools/maa/src/main/java/org/oceandsl/tools/maa/stages/SimilarMethodSetMergeInterfaceStage.java
+++ tools/maa/src/main/java/org/oceandsl/tools/maa/stages/SimilarMethodSetMergeInterfaceStage.java
@@ -1,8 +1,29 @@
+/***************************************************************************
+ * Copyright (C) 2023 OceanDSL (https://oceandsl.uni-kiel.de)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ***************************************************************************/
 package org.oceandsl.tools.maa.stages;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import org.eclipse.emf.common.util.EList;
 
@@ -15,31 +36,132 @@ public class SimilarMethodSetMergeInterfaceStage extends AbstractMergeInterfaceS
 
     private final int methodDistance;
 
+    /**
+     * Create interface merger.
+     *
+     * @param methodDistance
+     *            maximal distance if methods of two interfaces
+     */
     public SimilarMethodSetMergeInterfaceStage(final int methodDistance) {
+        super();
         this.methodDistance = methodDistance;
     }
 
     @Override
     protected void execute(final ModelRepository repository) throws Exception {
         final TypeModel typeModel = repository.getModel(TypePackage.Literals.TYPE_MODEL);
-        typeModel.getComponentTypes().values().forEach(componentType -> componentType.getProvidedInterfaceTypes()
-                .stream().map(iface -> this.computeDistances(iface, componentType.getProvidedInterfaceTypes()))).;
+        typeModel.getComponentTypes().values().forEach(componentType -> {
+            // find all provided interfaces of a component and compute their distance
+            final Stream<MethodDistance> distances = componentType.getProvidedInterfaceTypes().stream()
+                    .map(iface -> this.computeDistances(iface, componentType.getProvidedInterfaceTypes()))
+                    .reduce(new ArrayList<MethodDistance>(), (l, nl) -> this.joinList(l, nl)).stream()
+                    .filter(distance -> distance.distance <= this.methodDistance);
+            // group related interfaces
+            final Collection<Set<ProvidedInterfaceType>> groupedInterfaces = this.groupInterfaces(distances);
 
+            // merge them
+            // groupedInterfaces.forEach(set -> {
+            // System.out.println("------------");
+            // set.forEach(iface -> this.printIface(iface));
+            // });
+
+            // updated dependent required interfaces
+
+            // update assembly and deployment interfaces accordingly
+        });
+        this.outputPort.send(repository);
+
+    }
+
+    private void printIface(final ProvidedInterfaceType iface) {
+        System.out.println(">> " + iface.getSignature());
+        iface.getProvidedOperationTypes().values().forEach(op -> System.out.println("  " + op.getSignature()));
+    }
+
+    private Collection<Set<ProvidedInterfaceType>> groupInterfaces(final Stream<MethodDistance> distances) {
+        final Map<ProvidedInterfaceType, Set<ProvidedInterfaceType>> groupedInterfaces = new HashMap<>();
+
+        distances.forEach(distance -> {
+            if (groupedInterfaces.containsKey(distance.firstIface)) {
+                groupedInterfaces.get(distance.firstIface).add(distance.firstIface);
+                groupedInterfaces.get(distance.firstIface).add(distance.secondIface);
+            } else {
+                final Set<ProvidedInterfaceType> ifaceSet = new HashSet<>();
+                ifaceSet.add(distance.firstIface);
+                ifaceSet.add(distance.secondIface);
+                groupedInterfaces.put(distance.firstIface, ifaceSet);
+            }
+            if (groupedInterfaces.containsKey(distance.secondIface)) {
+                groupedInterfaces.get(distance.secondIface).add(distance.firstIface);
+                groupedInterfaces.get(distance.secondIface).add(distance.secondIface);
+            } else {
+                final Set<ProvidedInterfaceType> ifaceSet = new HashSet<>();
+                ifaceSet.add(distance.firstIface);
+                ifaceSet.add(distance.secondIface);
+                groupedInterfaces.put(distance.secondIface, ifaceSet);
+            }
+        });
+
+        final Collection<Set<ProvidedInterfaceType>> distinctSets = new ArrayList<>();
+        groupedInterfaces.values().forEach(set -> {
+            if (!this.containsSet(distinctSets, set)) {
+                distinctSets.add(set);
+            }
+        });
+
+        return distinctSets;
+    }
+
+    private boolean containsSet(final Collection<Set<ProvidedInterfaceType>> distinctSets,
+            final Set<ProvidedInterfaceType> set) {
+        return distinctSets.stream().anyMatch(distinct -> this.compareSet(distinct, set));
+    }
+
+    private boolean compareSet(final Set<ProvidedInterfaceType> distinct, final Set<ProvidedInterfaceType> set) {
+        return distinct.stream().allMatch(distinctElement -> set.contains(distinctElement));
+    }
+
+    private List<MethodDistance> joinList(final List<MethodDistance> list, final List<MethodDistance> secondList) {
+        secondList.forEach(element -> {
+            if (!this.isContainedInList(list, element)) {
+                list.add(element);
+            }
+        });
+        return list;
+    }
+
+    private boolean isContainedInList(final List<MethodDistance> list, final MethodDistance distance) {
+        return list.stream().anyMatch(
+                element -> element.firstIface == distance.firstIface && element.secondIface == distance.secondIface
+                        || element.firstIface == distance.secondIface && element.secondIface == distance.firstIface);
     }
 
     private List<MethodDistance> computeDistances(final ProvidedInterfaceType iface,
             final EList<ProvidedInterfaceType> providedInterfaceTypes) {
-        return providedInterfaceTypes.stream().map(secondIface -> this.computeDistance(iface, secondIface))
+        return providedInterfaceTypes.stream().filter(secondIface -> secondIface != iface)
+                .map(secondIface -> this.computeDistance(iface, secondIface))
                 .collect(Collectors.toCollection(ArrayList::new));
     }
 
     private MethodDistance computeDistance(final ProvidedInterfaceType firstIface,
             final ProvidedInterfaceType secondIface) {
-        return new MethodDistance(firstIface, secondIface,
-                this.countMissingMethods(firstIface, secondIface) + this.countMissingMethods(secondIface, firstIface));
+//        final double distance = (this.countMissingMethods(firstIface, secondIface)
+//                / firstIface.getProvidedOperationTypes().size()
+//                + this.countMissingMethods(secondIface, firstIface) / secondIface.getProvidedOperationTypes().size())
+//                / 2.0;
+        final double da = this.countMissingMethods(firstIface, secondIface);
+        final double db = this.countMissingMethods(secondIface, firstIface);
+
+        System.err.printf("distance 1>2 %6.3f 2>1 %6.3f %3d %3d\n", da, db,
+                firstIface.getProvidedOperationTypes().size(), secondIface.getProvidedOperationTypes().size());
+
+        final double distance = da + db;
+
+        return new MethodDistance(firstIface, secondIface, distance);
     }
 
-    private long countMissingMethods(final ProvidedInterfaceType firstIface, final ProvidedInterfaceType secondIface) {
+    private double countMissingMethods(final ProvidedInterfaceType firstIface,
+            final ProvidedInterfaceType secondIface) {
         return firstIface.getProvidedOperationTypes().values().stream()
                 .map(operation -> secondIface.getProvidedOperationTypes().containsValue(operation))
                 .filter(element -> !element).count();
@@ -49,10 +171,10 @@ public class SimilarMethodSetMergeInterfaceStage extends AbstractMergeInterfaceS
 
         private final ProvidedInterfaceType firstIface;
         private final ProvidedInterfaceType secondIface;
-        private final long distance;
+        private final double distance;
 
         public MethodDistance(final ProvidedInterfaceType firstIface, final ProvidedInterfaceType secondIface,
-                final long distance) {
+                final double distance) {
             this.firstIface = firstIface;
             this.secondIface = secondIface;
             this.distance = distance;
