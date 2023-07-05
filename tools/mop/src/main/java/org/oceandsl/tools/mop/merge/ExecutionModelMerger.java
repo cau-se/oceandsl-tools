25874a5e7699257526ead290a214bb8a1394058f
Reiner Jung <reiner.jung@email.uni-kiel.de>
2023-07-05 14:38:25 +0200

    Updated tooling to support operation dataflow in the merge tool.

--- tools/mop/src/main/java/org/oceandsl/tools/mop/merge/ExecutionModelMerger.java
+++ tools/mop/src/main/java/org/oceandsl/tools/mop/merge/ExecutionModelMerger.java
@@ -26,12 +26,13 @@ import kieker.model.analysismodel.execution.EDirection;
 import kieker.model.analysismodel.execution.ExecutionFactory;
 import kieker.model.analysismodel.execution.ExecutionModel;
 import kieker.model.analysismodel.execution.Invocation;
+import kieker.model.analysismodel.execution.OperationDataflow;
 import kieker.model.analysismodel.execution.StorageDataflow;
 import kieker.model.analysismodel.execution.Tuple;
 
 /**
  * @author Reiner Jung
- * @since
+ * @since 1.1.0
  */
 public final class ExecutionModelMerger {
 
@@ -41,6 +42,13 @@ public final class ExecutionModelMerger {
 
     /* default */ static void mergeExecutionModel(final DeploymentModel deploymentModel, // NOPMD
             final ExecutionModel targetModel, final ExecutionModel mergeModel) {
+        ExecutionModelMerger.mergeInvocations(deploymentModel, targetModel, mergeModel);
+        ExecutionModelMerger.mergeStorageDataflows(deploymentModel, targetModel, mergeModel);
+        ExecutionModelMerger.mergeOperationDataflows(deploymentModel, targetModel, mergeModel);
+    }
+
+    private static void mergeInvocations(final DeploymentModel deploymentModel, final ExecutionModel targetModel,
+            final ExecutionModel mergeModel) {
         for (final Entry<Tuple<DeployedOperation, DeployedOperation>, Invocation> entry : mergeModel.getInvocations()) {
             if (!ExecutionModelMerger.compareTupleOperationKeys(targetModel.getInvocations(), entry.getKey())) {
                 final Invocation value = ExecutionModelCloneUtils.duplicate(deploymentModel, entry.getValue());
@@ -50,41 +58,6 @@ public final class ExecutionModelMerger {
                 targetModel.getInvocations().put(key, value);
             }
         }
-        for (final Entry<Tuple<DeployedOperation, DeployedStorage>, StorageDataflow> entry : mergeModel
-                .getStorageDataflows()) {
-            final Tuple<DeployedOperation, DeployedStorage> targetModelKey = ExecutionModelMerger
-                    .findTupleStorageKeys(targetModel.getStorageDataflows(), entry.getKey());
-            if (targetModelKey == null) {
-                final StorageDataflow value = ExecutionModelCloneUtils.duplicate(deploymentModel, entry.getValue());
-                final Tuple<DeployedOperation, DeployedStorage> key = ExecutionFactory.eINSTANCE.createTuple();
-                key.setFirst(value.getCode());
-                key.setSecond(value.getStorage());
-                targetModel.getStorageDataflows().put(key, value);
-            } else {
-                final StorageDataflow targetStorageAccess = targetModel.getStorageDataflows().get(targetModelKey);
-                final StorageDataflow sourceStorageAccess = entry.getValue();
-                switch (sourceStorageAccess.getDirection()) {
-                case READ:
-                    if (targetStorageAccess.getDirection() == EDirection.WRITE) {
-                        targetStorageAccess.setDirection(EDirection.BOTH);
-                    }
-                    break;
-                case WRITE:
-                    if (targetStorageAccess.getDirection() == EDirection.READ) {
-                        targetStorageAccess.setDirection(EDirection.BOTH);
-                    }
-                    break;
-                case BOTH:
-                    targetStorageAccess.setDirection(EDirection.BOTH);
-                    break;
-                default:
-                    throw new InternalError(
-                            "Found unsupported direction type " + sourceStorageAccess.getDirection().getName());
-                }
-                targetModel.getStorageDataflows().put(targetModelKey, targetStorageAccess);
-            }
-        }
-
     }
 
     private static boolean compareTupleOperationKeys(
@@ -99,12 +72,101 @@ public final class ExecutionModelMerger {
         return false;
     }
 
+    private static void mergeStorageDataflows(final DeploymentModel deploymentModel, final ExecutionModel targetModel,
+            final ExecutionModel mergeModel) {
+        for (final Entry<Tuple<DeployedOperation, DeployedStorage>, StorageDataflow> entry : mergeModel
+                .getStorageDataflows()) {
+            final Tuple<DeployedOperation, DeployedStorage> targetModelKey = ExecutionModelMerger
+                    .findTupleStorageKeys(targetModel.getStorageDataflows(), entry.getKey());
+            if (targetModelKey == null) {
+                final StorageDataflow value = ExecutionModelCloneUtils.duplicate(deploymentModel, entry.getValue());
+                final Tuple<DeployedOperation, DeployedStorage> key = ExecutionFactory.eINSTANCE.createTuple();
+                key.setFirst(value.getCode());
+                key.setSecond(value.getStorage());
+                targetModel.getStorageDataflows().put(key, value);
+            } else {
+                final StorageDataflow targetStorageDataflow = targetModel.getStorageDataflows().get(targetModelKey);
+                final StorageDataflow sourceStorageDataflow = entry.getValue();
+                switch (sourceStorageDataflow.getDirection()) {
+                case READ:
+                    if (targetStorageDataflow.getDirection() == EDirection.WRITE) {
+                        targetStorageDataflow.setDirection(EDirection.BOTH);
+                    }
+                    break;
+                case WRITE:
+                    if (targetStorageDataflow.getDirection() == EDirection.READ) {
+                        targetStorageDataflow.setDirection(EDirection.BOTH);
+                    }
+                    break;
+                case BOTH:
+                    targetStorageDataflow.setDirection(EDirection.BOTH);
+                    break;
+                default:
+                    throw new InternalError(
+                            "Found unsupported direction type " + sourceStorageDataflow.getDirection().getName());
+                }
+                targetModel.getStorageDataflows().put(targetModelKey, targetStorageDataflow);
+            }
+        }
+    }
+
     private static Tuple<DeployedOperation, DeployedStorage> findTupleStorageKeys(
-            final EMap<Tuple<DeployedOperation, DeployedStorage>, StorageDataflow> storageDataflowes,
+            final EMap<Tuple<DeployedOperation, DeployedStorage>, StorageDataflow> storageDataflows,
             final Tuple<DeployedOperation, DeployedStorage> key) {
-        for (final Tuple<DeployedOperation, DeployedStorage> invocationKey : storageDataflowes.keySet()) {
+        for (final Tuple<DeployedOperation, DeployedStorage> invocationKey : storageDataflows.keySet()) {
             if (ModelUtils.isEqual(invocationKey.getFirst(), key.getFirst())
                     && ModelUtils.isEqual(invocationKey.getSecond(), key.getSecond())) {
+                return invocationKey;
+            }
+        }
+        return null;
+    }
+
+    private static void mergeOperationDataflows(final DeploymentModel deploymentModel, final ExecutionModel targetModel,
+            final ExecutionModel mergeModel) {
+        for (final Entry<Tuple<DeployedOperation, DeployedOperation>, OperationDataflow> entry : mergeModel
+                .getOperationDataflows()) {
+            final Tuple<DeployedOperation, DeployedOperation> targetModelKey = ExecutionModelMerger
+                    .findTupleOperationKeys(targetModel.getOperationDataflows(), entry.getKey());
+            if (targetModelKey == null) {
+                final OperationDataflow value = ExecutionModelCloneUtils.duplicate(deploymentModel, entry.getValue());
+                final Tuple<DeployedOperation, DeployedOperation> key = ExecutionFactory.eINSTANCE.createTuple();
+                key.setFirst(value.getCaller());
+                key.setSecond(value.getCallee());
+                targetModel.getOperationDataflows().put(key, value);
+            } else {
+                final OperationDataflow targetOperationDataflow = targetModel.getOperationDataflows()
+                        .get(targetModelKey);
+                final OperationDataflow sourceOperationDataflow = entry.getValue();
+                switch (sourceOperationDataflow.getDirection()) {
+                case READ:
+                    if (targetOperationDataflow.getDirection() == EDirection.WRITE) {
+                        targetOperationDataflow.setDirection(EDirection.BOTH);
+                    }
+                    break;
+                case WRITE:
+                    if (targetOperationDataflow.getDirection() == EDirection.READ) {
+                        targetOperationDataflow.setDirection(EDirection.BOTH);
+                    }
+                    break;
+                case BOTH:
+                    targetOperationDataflow.setDirection(EDirection.BOTH);
+                    break;
+                default:
+                    throw new InternalError(
+                            "Found unsupported direction type " + sourceOperationDataflow.getDirection().getName());
+                }
+                targetModel.getOperationDataflows().put(targetModelKey, targetOperationDataflow);
+            }
+        }
+    }
+
+    private static Tuple<DeployedOperation, DeployedOperation> findTupleOperationKeys(
+            final EMap<Tuple<DeployedOperation, DeployedOperation>, OperationDataflow> operationDataflows,
+            final Tuple<DeployedOperation, DeployedOperation> tuple) {
+        for (final Tuple<DeployedOperation, DeployedOperation> invocationKey : operationDataflows.keySet()) {
+            if (ModelUtils.isEqual(invocationKey.getFirst(), tuple.getFirst())
+                    && ModelUtils.isEqual(invocationKey.getSecond(), tuple.getSecond())) {
                 return invocationKey;
             }
         }
